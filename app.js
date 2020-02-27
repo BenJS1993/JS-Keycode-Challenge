989e76dfa07df74da761a964e661c498ce5e583c
BenJS1993 <32497552+BenJS1993@users.noreply.github.com>
2020-02-27 23:44:16 +0000

    background and border update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const keycodeTitle = document.getElementById('keycodeTitle');
 const keycodeGenerator = document.getElementById('keycodeGenerator');
 const head = document.getElementById('keycode');
+const touch = document.getElementById('keycodeTouch');
 
 function myKeycode(event) {
     var key = event.key;
@@ -10,7 +11,6 @@ function myKeycode(event) {
 
     head.textContent = which;
     
-
     document.getElementById('eventKey').innerHTML = key;
     document.getElementById('eventLocation').innerHTML = location;
     document.getElementById('eventCode').innerHTML = code;
@@ -19,5 +19,3 @@ function myKeycode(event) {
     keycodeGenerator.classList.remove('showKeycode');
     keycodeTitle.remove();
 }
-
-// return myKeycode = 'h ' + key + ' keyH ' + code + 72 + which;
